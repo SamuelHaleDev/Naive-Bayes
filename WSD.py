022743459e1fb5eb16c71dd4ab18d97051f84372
ww-samuel-hale <samuel.hale@woodwing.com>
2023-10-20 11:58:13 -0400

    Created objects to store the data

--- WSD.py
+++ WSD.py
@@ -16,6 +16,8 @@ def main():
     # Format data
     data = format_data(data)
     
+    data = convert_to_word_instances(data)
+    
     fold_generator = k_fold_split(5, data)
     
     for i, fold in enumerate(fold_generator):
@@ -36,6 +38,21 @@ class WordInstance:
         tokens = self.context.split()
         self.features = [(tokens[i], tokens[i+1]) for i in range(len(tokens)-1)]
             
+def convert_to_word_instances(data):
+    #['<instance id="plant.1000000" docsrc = "BNC/A07">\n', '<answer instance="plant.1000000" senseid="plant%factory"/>\n', '<context>\n', 'Until the mid- and late 1970s, there were simply no rules whereby groupings of parents could obtain a state-financed, multi-denominational school, as the only channels of communication in these matters were those between the Department of Education and the relevant diocesan department or other church board. For example, a new housing estate would be built, the diocese would be informed of the development at the planning stage, and the diocesan office would put in for a school, or an extension for an existing school. The arrangement had been unchanged since the early years of the state and had become entirely natural. Similarly for the protestant community: the school  <head>plant</head>  is owned by the church, or appropriate church body. \n', '</context>\n', '</instance>\n']
+    list_instances = []
+    for instance in data:
+        for i, line in enumerate(instance):
+            if line.startswith('<instance'):
+                instance_id = line.split('"')[1]
+            elif line.startswith('<answer'):
+                sense_id = line.split('"')[3]
+            elif line.startswith('<context>'):
+                context = instance[i+1].replace('\n', ' ')
+                head = context.split('<head>')[1].split('</head>')[0]
+                context = context.replace(' <head>' + head + '</head> ', '')
+        list_instances.append(WordInstance(instance_id, sense_id, context, head))
+    return list_instances
 
 def read_data(file_name):
     with open(file_name, 'r') as f:
